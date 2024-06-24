cb94b75e89f249ce7309ef539e69b13ad1636231
GamingCuber <thegamingcuber19@gmail.com>
2024-06-23 21:01:25 -0700

    Added Player Object and .editorconfig

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 5f;
+    public Rigidbody2D playerBody;
+    private Vector2 movementVector;
+
+    void Update()
+    {
+        movementVector.x = Input.GetAxisRaw("Horizontal");
+        movementVector.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        playerBody.MovePosition(playerBody.position + movementSpeed * movementVector * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public GameObject leftWave;
+    public GameObject rightWave;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
