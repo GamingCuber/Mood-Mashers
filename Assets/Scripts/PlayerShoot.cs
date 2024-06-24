9287510c02314e4120f4f34d86deff0e61f6d8de
GamingCuber <thegamingcuber19@gmail.com>
2024-06-24 13:39:20 -0700

    Implemented Basic Shooting every 3 seconds

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -6,10 +6,33 @@ public class PlayerShoot : MonoBehaviour
 {
     public GameObject leftWave;
     public GameObject rightWave;
+    public float secondsPerShoot = 0.0f;
+    public float frames = 3.0f;
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-
+        leftWave.SetActive(false);
+        rightWave.SetActive(false);
+        StartCoroutine(shootWaves());
     }
+
+    IEnumerator shootWaves()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(secondsPerShoot);
+
+            leftWave.SetActive(true);
+            rightWave.SetActive(true);
+
+            for (int i = 0; i < frames; i++)
+            {
+                yield return new WaitForEndOfFrame();
+            }
+
+            leftWave.SetActive(false);
+            rightWave.SetActive(false);
+        }
+    }
+
 }
