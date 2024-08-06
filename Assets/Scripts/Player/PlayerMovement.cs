85ca95087a878b9b8e69b1b31fb2545efd5e7551
Enkhamgalan Geoffrey Enkhsaikhan <Thegamingcuber19@gmail.com>
2024-08-06 14:23:19 -0700

    Added Player Idle Animation

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,24 +7,46 @@ public class PlayerMovement : MonoBehaviour
     public float movementSpeed = 5f;
     public Rigidbody2D playerBody;
     private Vector2 movementVector;
+    private Animator playerAnimator;
     public PauseMenuManager pauseMenu;
 
+
+    void Start()
+    {
+        playerAnimator = GetComponent<Animator>();
+    }
     void Update()
     {
         // Creates a movement vector based on player input
         movementVector.x = Input.GetAxisRaw("Horizontal");
         movementVector.y = Input.GetAxisRaw("Vertical");
+        // If the player isn't moving, plays idle animation
+        if (movementVector.x == 0 && movementVector.y == 0)
+        {
+            playerAnimator.SetBool("isMoving", false);
+        }
+        // If the player is moving, then plays walking animation
+        else
+        {
+            playerAnimator.SetBool("isMoving", true);
+        }
     }
 
     void FixedUpdate()
     {
         if (!pauseMenu.isPaused)
         {
+            // Normalizes movement vector, preventing faster movement when moving diagonally
             if (movementVector.magnitude > 1)
             {
                 movementVector = movementVector.normalized;
             }
             playerBody.MovePosition(playerBody.position + movementSpeed * Time.fixedDeltaTime * movementVector);
         }
+
     }
 }
+
+
+
+
