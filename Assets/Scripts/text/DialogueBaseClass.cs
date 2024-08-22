12a332cf4b18247a002eb65cd2184b7ffc9bebd0
AquaBlueToons <lopezroman19@yahoo.com>
2024-08-21 17:19:29 -0700

    v0.3.3.1 Basic story part added

--- Assets/Scripts/text/DialogueBaseClass.cs
+++ Assets/Scripts/text/DialogueBaseClass.cs
@@ -3,21 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.InputSystem;
 
 
 namespace DialogueSystem
 {
     public class DialogueBaseClass : MonoBehaviour
     {
-        protected IEnumerator WriteText(string input, TMP_Text textHolder)
+
+        public bool finished { get; private set; }
+
+
+        protected IEnumerator WriteText(string input, TMP_Text textHolder, float delay, float delayBetweenLines)
         {
            
             for (int i = 0; i < input.Length; i++)
             {
               
                 textHolder.text += input[i];
-                yield return new WaitForSeconds(0.1f);
+                yield return new WaitForSeconds(delay);
             }
+
+            //yield return new WaitForSeconds(delayBetweenLines);
+            yield return new WaitUntil(() => Input.anyKeyDown);
+            finished = true;
         }
     }
 }
--- Assets/Scripts/text/DialogueLine.cs
+++ Assets/Scripts/text/DialogueLine.cs
@@ -14,13 +14,27 @@ namespace DialogueSystem
 
         [Header("Text Option")]
         [SerializeField] private string input;
-     
+
+        [Header("Timing Text")]
+        [SerializeField] private float delay;
+        [SerializeField] private float delayBetweenLines;
+
+        [Header("Image")]
+        [SerializeField] private Sprite characterSprite;
+        [SerializeField] private Image imageholder;
 
         private void Awake()
         {
             textHolder = GetComponent<TMP_Text>();
+            textHolder.text = "";
 
-            var coroutine = StartCoroutine(WriteText(input, textHolder));
+            imageholder.sprite = characterSprite;
+            imageholder.preserveAspect = true;  
+        }
+
+        private void Start()
+        {
+            StartCoroutine(WriteText(input, textHolder, delay, delayBetweenLines)); 
         }
     }
 }
